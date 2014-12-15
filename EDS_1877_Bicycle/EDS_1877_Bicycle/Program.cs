7b517833a51644c19625a12972d4b52127ec5aee
ermakov-ds <ermakov.dm.s@yandex.ru>
2014-12-15 21:03:13 +0400

    1877

---
+++ EDS_1877_Bicycle/EDS_1877_Bicycle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDS_1877_Bicycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inp1 = int.Parse(Console.ReadLine());
+            int inp2 = int.Parse(Console.ReadLine());
+            if (((inp1 % 2 == 0) || (inp2 % 2 != 0)) )
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}

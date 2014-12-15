1ebd74ac1d2b0e5d5a37af7648ec3595ebdcfbbe
ermakov-ds <ermakov.dm.s@yandex.ru>
2014-12-15 18:54:58 +0400

    1000

---
+++ EDS_1000_ABProblem/EDS_1000_ABProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDS_1000_ABProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iRes = 0;
+            foreach (string str in Console.ReadLine().Split(' '))
+            {
+                iRes += int.Parse(str);
+            }
+            Console.Write(iRes);
+        }
+    }
+}

f76db3707b25ef09b9c150329789bf986e977584
ermakov-ds <ermakov.dm.s@yandex.ru>
2014-12-15 20:04:28 +0400

    1001

---
+++ EDS_1001_SquareRoot/EDS_1001_SquareRoot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDS_1001_SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Globalization.NumberFormatInfo nfi = System.Globalization.NumberFormatInfo.InvariantInfo;
+            List<string> strs = new List<string>();
+            string[] inputStr = Console.In.ReadToEnd().Split(new char[] { ' ', '\n', '\r', '\t' });
+            foreach (string str in inputStr)
+            {
+                if (str != string.Empty)
+                    strs.Add(str);
+            }
+            for (int i = strs.Count - 1; i > -1; i--)
+            {
+                double sqrt = Math.Sqrt(double.Parse(strs[i]));
+                Console.WriteLine(string.Format("{0:F4}", sqrt));
+            }
+        }
+    }
+}

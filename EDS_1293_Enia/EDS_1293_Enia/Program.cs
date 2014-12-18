dc2a04c369e6960e42a16b936886a5b528e3bdb1
ermakov-ds <ermakov.dm.s@yandex.ru>
2014-12-18 16:00:00 +0400

    1293

---
+++ EDS_1293_Enia/EDS_1293_Enia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDS_1293_Enia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = Console.ReadLine().Split(' ');
+            //List<int> Ns = new List<int>();
+            int res = 2;
+            foreach (string s in strs)
+                res *= int.Parse(s); 
+            Console.WriteLine(res);
+        }
+    }
+}

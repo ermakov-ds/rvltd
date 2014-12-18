85bd6bd8bb9b8571e06bc8e08d63d177aa808a26
ermakov-ds <ermakov.dm.s@yandex.ru>
2014-12-18 16:05:56 +0400

    1409

---
+++ EDS_1409_TwoBandits/EDS_1409_TwoBandits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDS_1409_TwoBandits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = Console.ReadLine().Split(' ');
+            List<int> i = new List<int>();
+            int iAll = 0;
+            foreach(string str in strs)
+            {
+                i.Add(int.Parse(str));
+                iAll += i.Last();
+            }
+            iAll--;
+            foreach (int iTmp in i)
+            {
+                Console.Write((iAll - iTmp).ToString() + ' ');
+            }
+            
+        }
+    }
+}
